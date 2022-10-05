fb5978fd6647d2ea5c77207cfcb244b430369ba7
Ashly-Ochwada <ashlywochwada@gmail.com>
2022-10-05 09:48:52 +0300

    edits

--- Digital_Wallet/Wallet/urls.py
+++ Digital_Wallet/Wallet/urls.py
@@ -1,50 +1,62 @@
 from django.urls import path
 from .import views
-from .views import list_customers,register_card, register_customer, register_loan, register_notification, register_receipt, register_reward, register_thirdparty, register_transaction, register_wallet, register_account
+from .views import edit_account, edit_cards, edit_customer, edit_loans, edit_notifications, edit_receipts, edit_rewards, edit_thirdparties, edit_transaction, list_customers,register_card, edit_wallet,register_customer, register_loan, register_notification, register_receipt, register_reward, register_thirdparty, register_transaction, register_wallet, register_account
 
 
 urlpatterns = [
     path("register/", register_customer, name = "registration"),
     path("customers/", list_customers, name = "customers_list"),
+    path("wallets/edit/<int:id>/",edit_customer,name="edit_customer"),
+
 
 
     path("register/", register_wallet, name="registration"),
     path("wallets/", views.list_wallets, name = "wallets_list"),
+    path("wallets/edit/<int:id>/",edit_wallet,name="edit_wallet"),
+
 
 
     path("register/", register_account, name="registration"),
     path("accounts/",views.list_accounts, name = "accounts_list"),
+    path("wallets/edit/<int:id>/",edit_account,name="edit_account"),
 
 
     path("register/", register_transaction, name="registration"),
     path("transactions/", views.list_transactions, name = "transactions_list"),
+    path("wallets/edit/<int:id>/",edit_transaction,name="edit_transaction"),
 
 
 
     path("register/", register_receipt, name="registration"),
     path("receipts/", views.list_receipts,name="receipts_list"),
+    path("wallets/edit/<int:id>/",edit_receipts,name="edit_receipts"),
 
 
     path("register/", register_card, name="registration"),
     path("cards/", views.list_cards,name="cards_list"),
+    path("wallets/edit/<int:id>/",edit_cards,name="edit_cards"),
 
 
 
     path("register/", register_thirdparty, name="registration"),
     path("thirdparties/", views.list_thirdparties,name="thirdparties_list"),
+    path("wallets/edit/<int:id>/",edit_thirdparties,name="edit_thirdparties"),
 
 
 
     path("register/", register_notification, name="registration"),
     path("notifications/", views.list_notifications,name="notifications_list"),
+    path("wallets/edit/<int:id>/",edit_notifications,name="edit_notifications"),
 
 
     path("register/", register_loan, name="registration"),
     path("loans/", views.list_loans,name="loans_list"),
+    path("wallets/edit/<int:id>/",edit_loans,name="edit_loans"),
 
 
     path("register/", register_reward, name="registration"),
      path("rewards/", views.list_rewards,name="rewards_list"),
+     path("wallets/edit/<int:id>/",edit_rewards,name="edit_rewards"),
 ]
 
 
--- Digital_Wallet/Wallet/views.py
+++ Digital_Wallet/Wallet/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # from Digital_Wallet.Wallet.models import Notification
-from .models import Account, Card, Customer, Notification, Receipt, Reward, Transaction, Wallet, Loan
+from .models import Account, Card, Customer, Notification, Receipt, Reward, Thirdparty, Transaction, Wallet, Loan
 # Create your views here.
 from .forms import AccountRegistrationForm, CardRegistrationForm, CustomerRegistrationForm,TransactionRegistrationForm, LoanRegistrationForm, NotificationRegistrationForm, ReceiptRegistrationForm, RewardRegistrationForm, ThirdpartyRegistrationForm, WalletRegistrationForm
 
@@ -24,6 +24,17 @@ def list_customers(request):
     return render(request,"wallet/customer_list.html",
     {"Customers": customers} )
 
+def edit_customer(request,id):
+    customer=Customer.objects.get(id=id)
+    if request.method =="POST":
+        form=CustomerRegistrationForm(request.POST,instance=Customer)
+        if form.is_valid():
+            form.save()
+            return redirect("customer_profile",id=customer.id)
+    else:
+        form=CustomerRegistrationForm(instance=Customer)
+        return render(request,"wallet/edit_customer.html",{"form":form})    
+
 # 
 
 def register_wallet(request):
@@ -40,8 +51,18 @@ def register_wallet(request):
 def list_wallets(request):
     wallets = Wallet.objects.all()
     return render(request,"wallet/wallet_list.html",
-    {"Wallets": wallets} )         
+    {"Wallets": wallets} )  
 
+def edit_wallet(request,id):
+    wallet=Wallet.objects.get(id=id)
+    if request.method =="POST":
+        form=WalletRegistrationForm(request.POST,instance=Wallet)
+        if form.is_valid():
+            form.save()
+            return redirect("wallet_profile",id=wallet.id)
+    else:
+        form=WalletRegistrationForm(instance=Wallet)
+        return render(request,"wallet/edit_wallet.html",{"form":form})
 
 
 def register_account(request):
@@ -58,7 +79,19 @@ def register_account(request):
 def list_accounts(request):
     accounts = Account.objects.all()
     return render(request,"wallet/account_list.html",
-    {"Accounts": accounts} )  
+    {"Accounts": accounts} ) 
+
+def edit_account(request,id):
+    account=Account.objects.get(id=id)
+    if request.method =="POST":
+        form=AccountRegistrationForm(request.POST,instance=Account)
+        if form.is_valid():
+            form.save()
+            return redirect("account_profile",id=account.id)
+    else:
+        form=AccountRegistrationForm(instance=Account)
+        return render(request,"wallet/edit_account.html",{"form":form})
+
 
 
 def register_transaction(request):
@@ -75,6 +108,16 @@ def list_transactions(request):
     transactions= Transaction.objects.all()
     return render(request,"wallet/transaction_list.html",
     {"Transactions": transactions} )              
+def edit_transaction(request,id):
+    transaction=Transaction.objects.get(id=id)
+    if request.method =="POST":
+        form=TransactionRegistrationForm(request.POST,instance=Transaction)
+        if form.is_valid():
+            form.save()
+            return redirect("transaction_profile",id=transaction.id)
+    else:
+        form=TransactionRegistrationForm(instance=Transaction)
+        return render(request,"wallet/edit_transaction.html",{"form":form})
 
 
 
@@ -91,7 +134,17 @@ def register_receipt(request):
 def list_receipts(request):
     receipts= Receipt.objects.all()
     return render(request,"wallet/receipt_list.html",
-    {"Receipts": receipts} )            
+    {"Receipts": receipts} )  
+def edit_receipts(request,id):
+    receipt=Receipt.objects.get(id=id)
+    if request.method =="POST":
+        form=ReceiptRegistrationForm(request.POST,instance=Receipt)
+        if form.is_valid():
+            form.save()
+            return redirect("receipt_profile",id=receipt.id)
+    else:
+        form=ReceiptRegistrationForm(instance=Receipt)
+        return render(request,"wallet/edit_receipt.html",{"form":form})              
 
 
 
@@ -109,6 +162,16 @@ def list_cards(request):
     cards= Card.objects.all()
     return render(request,"wallet/card_list.html",
     {"Cards": cards} )   
+def edit_cards(request,id):
+    card=Card.objects.get(id=id)
+    if request.method =="POST":
+        form=CardRegistrationForm(request.POST,instance=Receipt)
+        if form.is_valid():
+            form.save()
+            return redirect("card_profile",id=card.id)
+    else:
+        form=CardRegistrationForm(instance=Card)
+        return render(request,"wallet/edit_card.html",{"form":form}) 
 
 
 
@@ -126,7 +189,16 @@ def list_thirdparties(request):
     thirdparties= Card.objects.all()
     return render(request,"wallet/thirdparty_list.html",
     {"Thirdparties": thirdparties} )
-
+def edit_thirdparties(request,id):
+    thrirdparty=Thirdparty.objects.get(id=id)
+    if request.method =="POST":
+        form=ThirdpartyRegistrationForm(request.POST,instance=Thirdparty)
+        if form.is_valid():
+            form.save()
+            return redirect("thirdparty_profile",id=thrirdparty.id)
+    else:
+        form=ThirdpartyRegistrationForm(instance=Thirdparty)
+        return render(request,"wallet/edit_thirdparty.html",{"form":form}) 
 
 
 def register_notification(request):
@@ -143,7 +215,16 @@ def list_notifications(request):
     notifications= Notification.objects.all()
     return render(request,"wallet/notification_list.html",
     {"Notifications": notifications} )
-
+def edit_notifications(request,id):
+    notification=Notification.objects.get(id=id)
+    if request.method =="POST":
+        form=NotificationRegistrationForm(request.POST,instance=Notification)
+        if form.is_valid():
+            form.save()
+            return redirect("notification_profile",id=notification.id)
+    else:
+        form=NotificationRegistrationForm(instance=Notification)
+        return render(request,"wallet/edit_notification.html",{"form":form}) 
 
 
 
@@ -161,7 +242,16 @@ def list_loans(request):
     loans= Loan.objects.all()
     return render(request,"wallet/loan_list.html",
     {"Loans": loans} )
-
+def edit_loans(request,id):
+    loan=Loan.objects.get(id=id)
+    if request.method =="POST":
+        form=LoanRegistrationForm(request.POST,instance=Loan)
+        if form.is_valid():
+            form.save()
+            return redirect("loan_profile",id=loan.id)
+    else:
+        form=LoanRegistrationForm(instance=Loan)
+        return render(request,"wallet/edit_loan.html",{"form":form}) 
 
 
 def register_reward(request):
@@ -178,3 +268,13 @@ def list_rewards(request):
     rewards= Reward.objects.all()
     return render(request,"wallet/reward_list.html",
     {"Loans": rewards} )      
+def edit_rewards(request,id):
+    reward=Reward.objects.get(id=id)
+    if request.method =="POST":
+        form=RewardRegistrationForm(request.POST,instance=Reward)
+        if form.is_valid():
+            form.save()
+            return redirect("reward_profile",id=reward.id)
+    else:
+        form=RewardRegistrationForm(instance=Reward)
+        return render(request,"wallet/edit_reward.html",{"form":form})     
